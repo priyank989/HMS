b43599d7a6a7a4f29a03f99c79fe3eac461f93da
Logan Priyank <logan.priyank@ithands.biz>
2021-06-01 12:59:12 +0530

    changes

--- resources/views/patient/mbill.blade.php
+++ resources/views/patient/mbill.blade.php
@@ -17,7 +17,7 @@
     <div class="main-container">
     <div class="container">
 {{--        <div class="text-center logo">Logo</div>--}}
-    <form class="med-form" method="post"  action="{{ route('bill_payment') }}">
+    <form class="med-form" method="post"  action="{{ route('mbill_payment') }}">
         {{csrf_field()}}
         <div class="row">
             <div class="col-lg-8">
